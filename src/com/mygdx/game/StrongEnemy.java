675db6ec2c41312cb54662f1f07c107f142ee34e
donetch <115596546+donetch@users.noreply.github.com>
2024-06-28 00:22:49 -0400

    Update StrongEnemy.java

--- src/com/mygdx/game/StrongEnemy.java
+++ src/com/mygdx/game/StrongEnemy.java
@@ -2,23 +2,24 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import java.util.Iterator;
 
 public class StrongEnemy extends Enemy {
 
-    private static final int STRONG_ENEMY_HITS_TO_KILL = 2;
-
-    public StrongEnemy(int x, int y, int size, int xSpeed, int ySpeed, Texture texture) {
-        super(x, y, size, xSpeed, ySpeed, texture, STRONG_ENEMY_HITS_TO_KILL);
+    public StrongEnemy(int x, int y, int size, int xSpeed, int ySpeed, Texture texture, int health) {
+        super(x, y, size, xSpeed, ySpeed, texture, health);
     }
 
     @Override
-    public void update() {   
+    public void update(Llama llama) {   
         x += xSpeed;
         y += ySpeed;
 
         if (x + xSpeed < 0 || x + xSpeed + spr.getWidth() > Gdx.graphics.getWidth()) xSpeed *= -1;
         if (y + ySpeed < 0 || y + ySpeed + spr.getHeight() > Gdx.graphics.getHeight()) ySpeed *= -1;
         spr.setPosition(x, y);
+        
+        if(!llama.isInvulnerable()){
+            checkCollision(llama);
+        }
     }
 }
