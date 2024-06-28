a6f56f2f4df6b98dafea4e831a2f6d4acef3e7f4
LucianoFredes25 <127423251+LucianoFredes25@users.noreply.github.com>
2024-06-27 22:25:15 -0400

    Update Llama.java
    Agregar setInicio

--- src/com/mygdx/game/Llama.java
+++ src/com/mygdx/game/Llama.java
@@ -1,231 +1,188 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 
-public class Llama implements Colisionable {
+public class PantallaJuego extends Pantalla {
 
-    private static final float MAX_SPEED = 5f;
-    private static final float ACCELERATION = 0.1f;
-    private static final float DECELERATION = 0.05f;
-    private static final float BULLET_SPEED = 6f;
-    private static final int TIEMPO_HERIDO_MAX = 50;
-    
-    private static Llama instance;
+    private int score;
+    private int ronda;
+    private int velXEnemy; 
+    private int velYEnemy; 
+    private int cantEnemy;
+    private Texture backgroundImage;
+    private Music gameMusic;
+    private Llama llama;
+    private List<Enemy> enemies = new ArrayList<>();
+    private ArrayList<Bullet> balas = new ArrayList<>();
 
-    private boolean destruida = false;
-    private int vidas;
-    private float xVel = 0;
-    private float yVel = 0;
-    private Sprite spr;
-    private Sound sonidoHerido;
-    private Sound soundBala;
-    private Texture txBala;
-    private boolean herido = false;
-    private boolean invulnerable = false;
-    private int tiempoHerido;
-    private int tiempoInvulnerable = 5;
-
-    /**
-     * Constructor para la clase Llama.
-     */
-   private Llama(){
-        int y = 30;
-        int x = Gdx.graphics.getWidth() / 2 - 50;
-        this.sonidoHerido = Gdx.audio.newSound(Gdx.files.internal("hurt.ogg"));
-        this.soundBala = Gdx.audio.newSound(Gdx.files.internal("pop-sound.ogg"));
-        this.txBala = new Texture(Gdx.files.internal("Rocket2.png"));
-        this.spr = new Sprite(new Texture(Gdx.files.internal("MainShip3.png")));
-        this.spr.setPosition(x, y);
-        this.spr.setBounds(x, y, 45, 45);
-    }
-   
-    public static Llama getInstance() {
-        if (instance == null) {
-            instance = new Llama();
-        }
-        return instance;
-    }
-
-    /**
-     * Dibuja la llama en la pantalla y maneja su movimiento y disparo.
-     *
-     * @param batch El SpriteBatch utilizado para dibujar la llama.
-     * @param juego La instancia del juego que contiene la lógica del juego.
-     */
-    public void draw(SpriteBatch batch, PantallaJuego juego) {
-        float x = spr.getX();
-        float y = spr.getY();
-
-        if (!herido) {
-            handleMovement();
-            handleRotation();
-            keepWithinBounds();
-            spr.setPosition(x + xVel, y + yVel);
-            spr.draw(batch);
-        } else {
-            spr.setX(spr.getX() + MathUtils.random(-2, 2));
-            spr.draw(batch);
-            spr.setX(x);
-            tiempoHerido--;
-            if (tiempoHerido <= 0) herido = false;
-        }
-        if (invulnerable) {
-            tiempoInvulnerable--;
-            if (tiempoInvulnerable <= 0) {
-                invulnerable = false;
-            }
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            shoot(juego);
-        }
-    }
-
-    private void handleMovement() {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) xVel = Math.max(xVel - ACCELERATION, -MAX_SPEED);
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) xVel = Math.min(xVel + ACCELERATION, MAX_SPEED);
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) yVel = Math.max(yVel - ACCELERATION, -MAX_SPEED);
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) yVel = Math.min(yVel + ACCELERATION, MAX_SPEED);
-
-        if (!Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            xVel = xVel > 0 ? Math.max(xVel - DECELERATION, 0) : Math.min(xVel + DECELERATION, 0);
-        }
-        if (!Gdx.input.isKeyPressed(Input.Keys.DOWN) && !Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            yVel = yVel > 0 ? Math.max(yVel - DECELERATION, 0) : Math.min(yVel + DECELERATION, 0);
-        }
-    }
-
-    private void handleRotation() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) spr.setRotation(spr.getRotation() - 90);
-        if (Gdx.input.isKeyJustPressed(Input.Keys.E)) spr.setRotation(spr.getRotation() + 90);
-    }
-
-    private void keepWithinBounds() {
-        float x = spr.getX();
-        float y = spr.getY();
-
-        if (x + xVel < 0) {
-            xVel = 0;
-            x = 0;
-        }
-        if (x + xVel + spr.getWidth() > Gdx.graphics.getWidth()) {
-            xVel = 0;
-            x = Gdx.graphics.getWidth() - spr.getWidth();
-        }
-        if (y + yVel < 0) {
-            yVel = 0;
-            y = 0;
-        }
-        if (y + yVel + spr.getHeight() > Gdx.graphics.getHeight()) {
-            yVel = 0;
-            y = Gdx.graphics.getHeight() - spr.getHeight();
-        }
-
-        spr.setPosition(x + xVel, y + yVel);
-    }
-
-    private void shoot(PantallaJuego juego) {
-        float rotation = spr.getRotation();
-        float rad = (float) Math.toRadians(rotation);
-        int  bulletVelX = (int) (BULLET_SPEED * Math.cos(rad));
-        int bulletVelY =  (int) (BULLET_SPEED * Math.sin(rad));
-
-        float bulletX = (float) (spr.getX() + spr.getWidth() / spr.getHeight() / 2 * Math.cos(rad));
-        float bulletY = (float) (spr.getY() + spr.getHeight() / spr.getHeight() / 2 * Math.sin(rad));
-
-        Bullet bala = new Bullet(bulletX, bulletY, bulletVelX, bulletVelY, txBala);
-        juego.agregarBala(bala);
-        soundBala.play();
+    public PantallaJuego(SpaceNavigation game, int ronda, int vidas, int score,  
+        int velXEnemy, int velYEnemy, int cantEnemy) {
+        super(game);
+        this.ronda = ronda;
+        this.score = score;
+        this.velXEnemy = velXEnemy;
+        this.velYEnemy = velYEnemy;
+        this.cantEnemy = cantEnemy;
+        initializeEnemies(vidas);
+        Gdx.app.log("PantallaJuego", "PantallaJuego creada");
     }
 
     @Override
-    public void checkCollision(Colisionable b) {
-        if(!invulnerable){
-            if (!herido && b.getArea().overlaps(spr.getBoundingRectangle())) {
-                handleCollision(); 
+    protected void initializeAssets() {
+        backgroundImage = new Texture(Gdx.files.internal("lava.png"));
+        gameMusic = Gdx.audio.newMusic(Gdx.files.internal("piano-loops.wav"));
+        gameMusic.setLooping(true);
+        gameMusic.setVolume(0.5f);
+        gameMusic.play();
+    }
+
+    @Override
+    protected void initializeComponents() {
+        // Any additional components initialization
+    }
+
+    @Override
+    protected void renderComponents(float delta) {
+        batch.draw(backgroundImage, 0, 0, 1200, 800); // Ajusta las dimensiones según sea necesario
+        dibujaEncabezado();
+        if (!llama.estaHerido()) {
+            updateGameLogic();
+            checkLevelCompletion();
+        }
+        drawEntities();
+        checkGameOver();
+    }
+
+    private void initializeEnemies(int vidas) {
+        llama = Llama.getInstance();
+        llama.setInicio();
+
+        Random random = new Random();
+        for (int i = 0; i < cantEnemy; i++) {
+            if (random.nextBoolean()) {
+                enemies.add(new WeakEnemy(velXEnemy, velYEnemy));
+            } else {
+                enemies.add(new StrongEnemy(velXEnemy, velYEnemy));
             }
         }
     }
 
-    @Override
-    public void handleCollision() {
-        /*if (xVel == 0) xVel += b.getXSpeed() / 2;
-        if (b.getXSpeed() == 0) b.setXSpeed(b.getXSpeed() + (int) xVel / 2);
-        xVel = -xVel;
-        b.setXSpeed(-b.getXSpeed());
-
-        if (yVel == 0) yVel += b.getYSpeed() / 2;
-        if (b.getYSpeed() == 0) b.setYSpeed(b.getYSpeed() + (int) yVel / 2);
-        yVel = -yVel;
-        b.setYSpeed(-b.getYSpeed());*/
-
-        vidas--;
-        invulnerable = true;
-        tiempoInvulnerable = 100;
-        herido = true;
-        tiempoHerido = TIEMPO_HERIDO_MAX;
-        sonidoHerido.play();
-        if (vidas <= 0) destruida = true;
+    private void dibujaEncabezado() {
+        CharSequence str = "Vidas: " + llama.getVidas() + " Ronda: " + ronda;
+        game.getFont().getData().setScale(2f);
+        game.getFont().draw(batch, str, 10, 30);
+        game.getFont().draw(batch, "Score:" + this.score, Gdx.graphics.getWidth() - 150, 30);
+        game.getFont().draw(batch, "HighScore:" + game.getHighScore(), Gdx.graphics.getWidth() / 2 - 100, 30);
     }
 
-    public boolean estaDestruido() {
-        return !herido && destruida;
+    private void updateGameLogic() {
+        updateBullets();
+        updateEnemies(llama);
     }
 
-    public boolean estaHerido() {
-        return herido;
+    private void updateBullets() {
+        List<Bullet> bulletsToRemove = new ArrayList<>();
+        List<Enemy> enemiesToRemove = new ArrayList<>();
+
+        for (Bullet bullet : balas) {
+            bullet.update();
+            for (Enemy enemy : enemies) {
+                bullet.checkCollision(enemy);
+                if (bullet.isDestroyed()) {
+                    bulletsToRemove.add(bullet);
+                }
+                enemy.checkCollision(bullet);
+                if (enemy.isDead()) {
+                    enemiesToRemove.add(enemy);
+                    score += 10;
+                }
+            }
+        }
+
+        balas.removeAll(bulletsToRemove);
+        enemies.removeAll(enemiesToRemove);
     }
 
-    // Getters y Setters
+    private void updateEnemies(Llama llama) {
+        List<Enemy> enemiesToRemove = new ArrayList<>();
 
-    @Override
-    public Rectangle getArea() {
-        return spr.getBoundingRectangle();
-    }
+        for (Enemy enemy : enemies) {
+            enemy.update(llama);
+            llama.checkCollision(enemy);
+            if (enemy.isDead()) {
+                enemiesToRemove.add(enemy);
+                score += 10;
+            }
+        }
 
-    @Override
-    public int getXSpeed() {
-        return (int) xVel;
-    }
-
-    @Override
-    public void setXSpeed(int xSpeed) {
-        this.xVel = xSpeed;
-    }
-
-    @Override
-    public int getYSpeed() {
-        return (int) yVel;
-    }
-
-    @Override
-    public void setYSpeed(int ySpeed) {
-        this.yVel = ySpeed;
-    }
-
-    public int getVidas() {
-        return vidas;
-    }
-
-    public void setVidas(int vidas) {
-        this.vidas = vidas;
-    }
-
-    public int getX() {
-        return (int) spr.getX();
-    }
-
-    public int getY() {
-        return (int) spr.getY();
+        enemies.removeAll(enemiesToRemove);
     }
     
-    public boolean isInvulnerable() {
-        return invulnerable;
+    public boolean agregarBala(Bullet bullet) {
+        return balas.add(bullet);
+    }
+    
+    private void checkLevelCompletion() {
+        if (enemies.isEmpty()) {
+            ronda++;
+            llama.setVidas(llama.getVidas() + 3);
+            velXEnemy++;
+            velYEnemy++;
+            cantEnemy += 5;
+
+            Gdx.app.log("PantallaJuego", "Nivel completado, avanzando al siguiente nivel");
+            PantallaJuego nextScreen = new PantallaJuego(game, ronda, llama.getVidas(), score, velXEnemy, velYEnemy, cantEnemy);
+            nextScreen.resize(1200, 800);
+            game.setScreen(nextScreen);
+            dispose();
+        }
+    }
+
+    private void drawEntities() {
+        for (Enemy enemy : enemies) {
+            enemy.draw(batch);
+        }
+        for (Bullet bullet : balas) {
+            bullet.draw(batch);
+        }
+        llama.draw(batch, this);
+    }
+
+    private void checkGameOver() {
+        if (llama.estaDestruido()) {
+            Gdx.app.log("PantallaJuego", "Juego terminado, mostrando pantalla de Game Over");
+            if (score > game.getHighScore()) {
+                game.setHighScore(score);
+            }
+            PantallaGameOver gameOverScreen = new PantallaGameOver(game);
+            gameOverScreen.resize(1200, 800);
+            game.setScreen(gameOverScreen);
+            dispose();
+        }
+    }
+
+    @Override
+    protected void handleInput() {
+        // Implementación de manejo de entrada si es necesaria
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.log("PantallaJuego", "Escape key pressed");
+            game.setScreen(new PantallaMenu(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        Gdx.app.log("PantallaJuego", "Disposing PantallaJuego");
+        super.dispose();
+        gameMusic.dispose();
+        backgroundImage.dispose();
     }
 }
