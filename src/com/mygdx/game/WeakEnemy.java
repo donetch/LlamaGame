e45db32f98c6fc6a26d57ab77cf937e55721ac5b
donetch <115596546+donetch@users.noreply.github.com>
2024-06-28 00:22:29 -0400

    Update WeakEnemy.java

--- src/com/mygdx/game/WeakEnemy.java
+++ src/com/mygdx/game/WeakEnemy.java
@@ -9,23 +9,22 @@ import java.util.Iterator;
 import java.util.List;
 
 public class WeakEnemy extends Enemy {
-    private static final int HITS_TO_KILL = 1;
-    private static final float SHOOT_COOLDOWN = 1.0f;
+    private static final float SHOOT_COOLDOWN = 2.0f;
     private static final int BULLET_SPEED_Y = -2;
-
-    private List<Bullet> bullets;
-    private Texture bulletTexture;
+    
+    private static final Texture bulletTexture = new Texture(Gdx.files.internal("rocket3.png"));
+    private List<Bullet> bulletsWE;
     private float timeSinceLastShot;
 
-    public WeakEnemy(int x, int y, int size, int xSpeed, int ySpeed, Texture tx, Texture bulletTx) {
-        super(x, y, size, xSpeed, ySpeed, tx, HITS_TO_KILL);
-        this.bulletTexture = bulletTx;
-        this.bullets = new ArrayList<>();
+    public WeakEnemy(int x, int y, int size, int xSpeed, int ySpeed, Texture texture, int health) {
+        super(x, y, size, xSpeed, ySpeed, texture, health);
+        this.bulletsWE = new ArrayList<>();
         this.timeSinceLastShot = 0;
     }
 
+
     @Override
-    public void update() {
+    public void update(Llama llama) {
                 
         x += xSpeed;
         y += ySpeed;
@@ -40,34 +39,40 @@ public class WeakEnemy extends Enemy {
             timeSinceLastShot = 0;
         }
 
-        for (Iterator<Bullet> it = bullets.iterator(); it.hasNext(); ) {
+        for (Iterator<Bullet> it = bulletsWE.iterator(); it.hasNext(); ) {
             Bullet bullet = it.next();
             bullet.update();
+            if(!llama.isInvulnerable()){
+                llama.checkCollision(bullet);
+            }
         }
         
-        checkColllion();
+        if(!llama.isInvulnerable()){
+           checkCollision(llama);   
+        }
+        
     }
 
     @Override
     public void draw(SpriteBatch batch) {
         super.draw(batch);
-        for (Bullet bullet : bullets) {
+        for (Bullet bullet : bulletsWE) {
             bullet.draw(batch);
         }
     }
 
     private void shoot() {
         // Crear una bala que se mueva hacia abajo
-        bullets.add(new Bullet(
-            spr.getX() + spr.getWidth() / 2, // Posición x
-            spr.getY(), // Posición y
+        bulletsWE.add(new Bullet(
+            spr.getX() + spr.getWidth() / 2, // PosiciÃ³n x
+            spr.getY(), // PosiciÃ³n y
             0, // Velocidad x
             BULLET_SPEED_Y, // Velocidad y (hacia abajo)
             bulletTexture // Textura de la bala
         ));
     }
-
+    
     public List<Bullet> getBullets() {
-        return bullets;
+        return bulletsWE;
     }
 }
